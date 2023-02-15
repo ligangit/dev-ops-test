a54a5146bb7e79028f3e3509d9fe00c60e7eb5d7
ligang <ligang@szltech.com>
2023-02-15 14:11:45 +0800

    DevOps--demo

---
+++ src/main/java/com/ligangit/devopstest/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.ligangit.devopstest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello(String name) {
+        return "hello " + name;
+    }
+
+}
